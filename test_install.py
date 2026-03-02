c98ea3c3f602436772c60fedf7ee4bd992279ac0
mikkel-coder <mikkel.ks.sorensen@gmail.com>
2026-03-02 20:29:00 +0000

    l01: exercises done

---
+++ test_install.py
@@ -0,0 +1,13 @@
+import numpy
+import matplotlib
+import scipy
+import numba
+import pytest
+import dask
+
+print(f"{numpy.__version__=}")
+print(f"{matplotlib.__version__=}")
+print(f"{scipy.__version__=}")
+print(f"{numba.__version__=}")
+print(f"{pytest.__version__=}")
+print(f"{dask.__version__=}")
