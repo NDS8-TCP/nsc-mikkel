1a6d4b1d9f5bcccffc408e777198b481bb018491
mikkel-coder <mikkel.ks.sorensen@gmail.com>
2026-03-02 20:40:37 +0000

    l02: milestone 2 done

--- mandelbrot.py
+++ mandelbrot.py
@@ -57,3 +57,30 @@ def compute_mandelbrot(
         img.append(img_row)
 
     return img
+
+
+def compute_mandelbrot_numpy(
+    x_min: float,
+    x_max: float,
+    y_min: float,
+    y_max: float,
+    width: int,
+    height: int,
+    max_iter: int
+):
+    # Inspired by slide 30, lecture 02
+    x = np.linspace(x_min, x_max, width)
+    y = np.linspace(y_min, y_max, height)
+
+    X, Y = np.meshgrid(x, y)
+
+    C = X + 1j*Y
+
+    Z = np.zeros_like(C)
+    M = np.zeros_like(C, dtype=int)
+    for _ in range(max_iter):
+        mask = np.abs(Z) <= 2
+        Z[mask] = Z[mask]**2 + C[mask]
+        M[mask] += 1
+
+    return M
